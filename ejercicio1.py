dc91b7b9b690eec8b309b6acb9b2a61aea5cdafc
Nancy Guadalupe Robles Trejo <nanytr259@gmail.com>
2024-10-10 09:55:07 -0600

    01 Primer ejercicio

---
+++ ejercicio1.py
@@ -0,0 +1,14 @@
+""""
+Ejercicio1:
+  Escribir un progrtama que pregunte al usuario su nombre 
+  e imprimir un saludo
+Entradas:
+  nombre: string
+Salida: 
+saludo: string    
+"""
+nombre = input("Escribe tu nombre: ")
+edad = input("Escribe tu edad: \n")
+saludo = "Holaa " + nombre + " de " + edad + " años !"
+
+print(saludo)
