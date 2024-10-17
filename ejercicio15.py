be98d3c392d7a51a0e0270e29ac03c384f47ecfb
Nancy Guadalupe Robles Trejo <nanytr259@gmail.com>
2024-10-17 09:24:57 -0600

    ejercicio.15

---
+++ ejercicio15.py
@@ -0,0 +1,19 @@
+"""
+Dadas dos variables numéricas A y B, que el usuario debe teclear, se pide 
+realizar un algoritmo que intercambie los valores de ambas variables y muestre 
+cuanto valen al final las dos variables
+Entrada:
+  valor a:int
+  valor b:int 
+Salida:
+  intercambio de valores
+"""
+valorA = input("Ingresa el valor de A: ")
+valorA = int(valorA)
+valorB = input("Ingresa el valor de B: ")
+valorB = int(valorB)
+nx = valorA
+n1 = valorB
+ne = nx
+print("Ahora el valor de A es:",n1)
+print("Ahora el valor de B es:",nx)
